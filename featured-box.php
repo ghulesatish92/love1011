db2af74e3f05203dde37d1de84d8b791545e5854
ghulesatish92 <ghulesatish92@users.noreply.github.com>
2014-12-17 17:45:14 +0530

    Create featured-box.php

---
+++ featured-box.php
@@ -0,0 +1,35 @@
+<?php
+/* 	Design Theme's Featured Box to show the Featured Items of Front Page
+	Copyright: 2012-2014, D5 Creation, www.d5creation.com
+	Based on the Simplest D5 Framework for WordPress
+	Since Design 1.0
+*/
+?>
+
+<div class="featured-boxs">
+
+<div class="featured-box">
+<img src="<?php echo esc_url(of_get_option('featured-image1', get_template_directory_uri() . '/images/featured-image1.jpg')) ?>"
+<h2>Aldus PageMaker including version of lorem Ipsum.</h2>
+<p>Losen ipsum dolor sir.amet consectur adipising elit.Aenean et sollicitdin risus.</p>
+</img>
+</div>
+
+<div class="featured-box">
+<img src="<?php echo esc_url(of_get_option('featured-image2', get_template_directory_uri() . '/images/featured-image2.jpg')) ?>"
+<h2>Many desktop publishin packages and wb page.</h2>
+<p>It is a long established fact that a reader will be distracted by the readable content.</p>
+</img>
+
+</div>
+
+<div class="featured-box">
+<img src="<?php echo esc_url(of_get_option('featured-image3', get_template_directory_uri() . '/images/featured-image3.jpg')) ?>"
+<h2>Aldus PageMaker including version of lorem Ipsum.</h2>
+<p>Losen ipsum dolor sir.amet consectur adipising elit.Aenean et sollicitdin risus.</p>
+</img>
+</div>
+
+
+</div>
+
