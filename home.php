61cd05064aa8419ec8cdcac8a9f304491b451a80
ghulesatish92 <ghulesatish92@users.noreply.github.com>
2014-12-17 17:52:22 +0530

    Create home.php

---
+++ home.php
@@ -0,0 +1,8 @@
+<?php 
+/* 	Template Name: Blog
+	Design Theme's Blog Posts Showung Template
+	Copyright: 2012-2014, D5 Creation, www.d5creation.com
+	Based on the Simplest D5 Framework for WordPress
+	Since Design 1.2.6
+*/
+
